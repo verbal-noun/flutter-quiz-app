b26829c9176a1827954c58e6d13bc15a724c2306
verbal-noun <kaifahsan420@gmail.com>
2019-09-05 00:38:28 +1000

    Creating main function and build context

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,13 @@
 import 'package:flutter/material.dart';
 
+void main () {
+
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("Hello!"),
+    );
+  }
+}
